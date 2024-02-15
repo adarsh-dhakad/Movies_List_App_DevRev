650fd1fcad4cc1c8a80516ff64e0d71f2ec2cb6b
Adash Dhakad <dhakadadarsh1@gmail.com>
2024-02-15 13:42:05 +0530

    removed unused variables from MovieUi.kt

--- app/src/main/java/com/devrev/app/model/MovieUi.kt
+++ app/src/main/java/com/devrev/app/model/MovieUi.kt
@@ -6,13 +6,7 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class MovieUi(
     val id: Int,
-    val isAdultOnly: Boolean,
-    val popularity: Double,
-    val voteAverage: Double,
-    val voteCount: Int,
     val image: String,
     val title: String,
-    val overview: String,
-    val releaseDate: String,
-    val originalLanguage: String
+    val createdAt:Int,
 ) : Parcelable
